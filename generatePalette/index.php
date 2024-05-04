6f388fb73fcb19559f13490b2b0a332ef6503721
JorgeMiraFlores <130872239+JorgeMiraFlores@users.noreply.github.com>
2024-05-04 14:26:52 -0600

    Add files via upload

--- generatePalette/index.php
+++ generatePalette/index.php
@@ -11,13 +11,15 @@
     <div>
 <?php
 //Para ingresar la cantidad deseada :D
-if (isset($_POST['num'])){
-    $cant = $_POST['num'];
+if (isset($_POST['numR'])){
+    $cant = $_POST['numR'];
 }else{
     $cant = 5;
 }
 
+    
 for($j = 0; $j < $cant; $j++){
+    //Generar los colores
     $r = rand(0, 255);
     $g = rand(0, 255);
     $b = rand(0, 255);
@@ -27,15 +29,77 @@ for($j = 0; $j < $cant; $j++){
     $blueArray [$j] = $b;
 }
 
-rsort ($redArray);
-rsort ($greenArray);
-rsort ($blueArray);
 
+//Se comprueba si se ha seleccionado el checkbox RGB
+
+//Checkear de menor a mayor
+if(isset($_REQUEST['checkR'])) {
+$checkR = "checked";
+rsort ($redArray);
+echo "<script> console.log('Rojo Sort mayor a menor') </script>";
+}else{$checkR = "";}
+if(isset($_REQUEST['checkG'])) {
+$checkG = "checked";
+rsort ($greenArray);
+echo "<script> console.log('Verde Sort mayor a menor') </script>";
+}else{$checkG = "";}
+if(isset($_REQUEST['checkB'])) {
+$checkB = "checked";
+rsort ($blueArray);
+echo "<script> console.log('Azul Sort mayor a menor') </script>";
+}else{$checkB = "";}
+//Checkear de menor a mayor
+if(isset($_REQUEST['checkRR'])) {
+$checkRR = "checked";
+sort ($redArray);
+echo "<script> console.log('Rojo Sort menor a mayor') </script>";
+}else{$checkRR = "";}
+if(isset($_REQUEST['checkGR'])) {
+$checkGR = "checked";
+sort ($greenArray);
+echo "<script> console.log('Verde Sort menor a mayor') </script>";
+}else{$checkGR = "";}
+if(isset($_REQUEST['checkBR'])) {
+$checkBR = "checked";
+sort ($blueArray);
+echo "<script> console.log('Verde Sort menor a mayor') </script>";
+}else{$checkBR = "";}
+    
+
+//Crear un array para concatenar los colores
 for($j = 0; $j < $cant; $j++){
-    //Concatenar los colores
+    //Se concatenan los colores
     $color = "RGB($redArray[$j], $greenArray[$j], $blueArray[$j])";
+    //Guardar los colores en un array
     $colores[$j] = $color;   
 }
+?>
+</div>
+<div>
+    <form action="index.php?<?php echo $cant ?>" method="POST">
+        <label for="num" style="color: white; font-size: 15px"><b>Ingrese la cantidad<br> de colores:</b></label><br><br>
+        <input type="number" id="num" name="numR" min="2" max="10" value="<?php echo "$cant"; ?>" required> <br><br>
+
+        <div class="checkbox-group">
+        <input type="checkbox" class="checkboxRGB" id="checkboxR" name="checkR" value="check" <?php echo "$checkR"; ?>> <label for="checkbox" style="color: white; font-size: 15px"><b>Rojo Sort mayor a menor</b></label><br><br>
+        <input type="checkbox" class="checkboxRGB" id="checkboxG" name="checkG" value="check" <?php echo "$checkG"; ?>> <label for="checkbox" style="color: white; font-size: 15px"><b>Verde Sort mayor a menor</b></label><br><br>
+        <input type="checkbox" class="checkboxRGB" id="checkboxB" name="checkB" value="check" <?php echo "$checkB"; ?>> <label for="checkbox" style="color: white; font-size: 15px"><b>Azul Sort mayor a menor</b></label><br><br>
+        </div>
+
+        <!--Generar de menor a llayor-->
+        <div class="checkbox-group">
+        <input type="checkbox" class="checkboxRGBR" id="checkboxRR" name="checkRR" value="check" <?php echo "$checkRR"; ?>> <label for="checkbox" style="color: white; font-size: 15px"><b>Rojo Sort menor a mayor</b></label><br><br>
+        <input type="checkbox" class="checkboxRGBR" id="checkboxGR" name="checkGR" value="check" <?php echo "$checkGR"; ?>> <label for="checkbox" style="color: white; font-size: 15px"><b>Verde Sort menor a mayor</b></label><br><br>
+        <input type="checkbox" class="checkboxRGBR" id="checkboxBR" name="checkBR" value="check" <?php echo "$checkBR"; ?>> <label for="checkbox" style="color: white; font-size: 15px"><b>Azul Sort menor a mayor</b></label><br><br>
+        </div>
+
+        <input type="submit" value="Generar" 
+       title="Atheros Learning Platform"
+       class='cta-button' >
+    </form>
+</div>
+<?php
+//Imprimir los colores
 for($i = 0; $i < $cant; $i++){
     echo "
     <div class='parent'>
@@ -49,18 +113,8 @@ for($i = 0; $i < $cant; $i++){
 }
 
 ?>
-</div>
-<div>
-    <form action="index.php?<?php echo $cant ?>" method="POST">
-        <label for="num" style="color: white; font-size: 15px"><b>Ingrese la cantidad<br> de colores:</b></label><br><br>
-        <input type="number" id="num" name="num" min="2" max="10" value="<?php $cant ?>" required> <br><br>
-        <input type="submit" value="Generar" 
-       title="Atheros Learning Platform"
-       class='cta-button' >
-    </form>
-</div>
 
-<p style="color: white">Made by: Jorge Mira <br><b>Version: 1.2 &#169;</b></p>
+<p style="color: white">Made by: Jorge Mira <br><b>Version: 3.0 &#169;</b></p>
 
 <script src="script.js"></script>
 </body>
--- generatePalette/script.js
+++ generatePalette/script.js
@@ -1,14 +1,42 @@
 function myFunction() {
-    // Get the text field
+    // Obtener el valor del input
     var copyText = document.getElementById("myInput");
   
-    // Select the text field
+    // Seleccionar el texto
     copyText.select();
     copyText.setSelectionRange(0, 99999); // For mobile devices
   
-     // Copy the text inside the text field
+     // Copiar el texto
     navigator.clipboard.writeText(copyText.value);
   
-    // Alert the copied text
+    // Mostrar un mensaje de confirmación
     alert("Copied the text: " + copyText.value);
   }
+
+  //Alternar entre los rojos de la paleta
+  document.getElementById("checkboxR").onclick = function() {
+    document.getElementById("checkboxRR").checked = false;
+  };
+  document.getElementById("checkboxRR").onclick = function() {
+    document.getElementById("checkboxR").checked = false;
+  };
+  
+  //Alternar entre los verdes de la paleta
+  document.getElementById("checkboxG").onclick = function() {
+    document.getElementById("checkboxGR").checked = false;
+  };
+  document.getElementById("checkboxGR").onclick = function() {
+    document.getElementById("checkboxG").checked = false;
+  };
+    //Alternar entre los azules de la paleta
+    document.getElementById("checkboxB").onclick = function() {
+      document.getElementById("checkboxBR").checked = false;
+    };
+    document.getElementById("checkboxBR").onclick = function() {
+      document.getElementById("checkboxB").checked = false;
+    };
+    
+  
+  
+  
+  
